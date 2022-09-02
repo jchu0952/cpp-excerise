5069135b8f5572edf856f53211e781ea4ef6c90a
jchu0952 <86311908+jchu0952@users.noreply.github.com>
2022-09-03 07:35:05 +0800

    excerise5

---
+++ excerise5.cpp
@@ -0,0 +1,15 @@
+//excerise5
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool result1 = "sparrow" > "eagle";
+    bool result2 = 50<50/2+5*3 or 20-45/5-10 >=1;
+    bool result3 = "dog" > "Cat" and 45%3 ==0;
+    cout << boolalpha << result1 << endl;
+    cout << boolalpha << result2 << endl;
+    cout << boolalpha << result3 << endl;
+    return 0;
+}
