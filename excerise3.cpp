227f3a55019d96b162a251eeac162be625e3bbae
jchu0952 <86311908+jchu0952@users.noreply.github.com>
2022-09-03 07:34:23 +0800

    excerise3

---
+++ excerise3.cpp
@@ -0,0 +1,13 @@
+//excerise 3
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int price;
+    cout << "How much Nt is the USD?";
+    cin >> price;
+    cout << price<< " USD is "<< price*30 << " NT";
+    return 0;
+}
