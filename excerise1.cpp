093964a13d3a36e60b2f9bcff1bd594ac7c124a5
jchu0952 <86311908+jchu0952@users.noreply.github.com>
2022-09-03 07:33:23 +0800

    excerise1

---
+++ excerise1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0;i<=1;i++)
+    {
+        cout << "Welcome to your first day of c++ class!\n\n";
+    }
+    return 0;
+}
