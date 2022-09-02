96a6be76b4c4bcb519dbe31d6f68d3883e90ca1c
jchu0952 <86311908+jchu0952@users.noreply.github.com>
2022-09-03 07:34:01 +0800

    excerise2

---
+++ excerise2.cpp
@@ -0,0 +1,13 @@
+//excerise2
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<< "My name is Leon.\n";
+    cout << "There is one brother in my family.\n";
+    cout << "I like McDonald's Coke\n";
+    cout << "My favorite song is something just like this.\n";
+    return 0;
+}
